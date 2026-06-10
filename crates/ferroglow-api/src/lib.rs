22b5f6f90d4ff9c87767798d3fe4830e7bfddf13
Nathan Norton <notnortron@gmail.com>
2026-06-10 01:04:56 -0700

    Add pixel map endpoint

--- crates/ferroglow-api/src/lib.rs
+++ crates/ferroglow-api/src/lib.rs
@@ -1,10 +1,11 @@
 use axum::{Router, routing::get, Json};
-use ferroglow_models::Pattern;
+use ferroglow_models::{Color, Pattern, Pixel, PixelMap};
 
 pub fn router() -> Router {
     Router::new()
         .route("/health", get(health))
         .route("/patterns", get(patterns))
+        .route("/pixel_map", get(pixel_map))
 }
 
 async fn health() -> &'static str {
@@ -13,4 +14,9 @@ async fn health() -> &'static str {
 
 async fn patterns() -> Json<Vec<Pattern>> {
     Json(vec![Pattern::new("Test Pattern".to_string())])
+}
+
+async fn pixel_map() -> Json<Vec<PixelMap>> {
+    let color: Color = Color::new(0,0,0);
+    Json(vec![PixelMap::new(vec![Pixel::new(0, 0, color, 0, 0)])])
 }
--- crates/ferroglow-models/src/lib.rs
+++ crates/ferroglow-models/src/lib.rs
@@ -11,3 +11,48 @@ impl Pattern {
         Self { name }
     }
 }
+
+#[derive(Serialize)]
+pub struct Pixel {
+    x: u16,
+    y: u16,
+    color: Color,
+    universe: u16,
+    channel_position: u16,
+}
+
+impl Pixel {
+    pub fn new(
+        x:u16, 
+        y:u16, 
+        color: Color, 
+        universe: u16,
+        channel_position: u16,
+    ) -> Self {
+        Self { x, y, color, universe, channel_position }
+    }
+}
+
+#[derive(Serialize)]
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self {r, g, b}
+    }
+}
+
+#[derive(Serialize)]
+pub struct PixelMap {
+    pixels: Vec<Pixel>,
+}
+
+impl PixelMap {
+    pub fn new(pixels: Vec<Pixel>) -> Self {
+        Self { pixels }
+    }
+}
